6ba3d7572542236db3133ead6f0f5682aa68604c
Brage <bragearn@stud.ntnu.no>
2020-03-09 11:07:24 +0100

    inpaiting kinda works

--- src/preview.py
+++ src/preview.py
@@ -3,18 +3,20 @@ import blurring
 import numpy as np
 from PIL import Image
 import argparse
+import inpaiting
 parser = argparse.ArgumentParser(description='vitprog')
 parser.add_argument('function', type=str,
                     help='The function to preview')
 parser.add_argument('--image', type=str,
 					help='(optional) image to test on (default = lena).')
+parser.add_argument('--color', type=int,
+					help='(optional) color gray or color')
 args = parser.parse_args()
 #print(args.function)
 #print(args.image)
 
-def preview(function_def):
-	image = function_def("test_images/lena.png", True)
-
+def preview(image):
+#	image = function_def("test_images/lena.png", True)
 	plt.ion()
 	if(image.color):
 		plt.imshow(image.data)
@@ -22,8 +24,9 @@ def preview(function_def):
 		plt.imshow(image.data, plt.cm.gray)
 
 	plt.draw()
-
-	epoch = 1
+	epoch = 0
+	plt.title("Epoch {} (Alpha = {})".format(epoch, image.alpha))			
+	plt.pause(1)
 	while True:
 		try:
 			image.fit(1)
@@ -41,9 +44,16 @@ def preview(function_def):
 	plt.close()
 
 if __name__ == "__main__":
+	image_path = "test_images/lena.png" if args.image is None else args.image
+	color = True if args.color is None else bool(args.color)
+	print(color, args.color)
 	if(args.function == "blur"):
-		preview(blurring.blur)
+		image = blurring.blur(image_path, color)
+		image.add_nosise()
+		preview(image)
+	elif(args.function == "inpaiting"):
+		image = inpaiting.inpait(image_path, color)
+	#	image.add_nosise()
+		preview(image)
 	else:
 		raise NotImplementedError
-
-
