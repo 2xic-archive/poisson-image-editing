b8150c385ba1a567e890e2b04cf500a45afac576
Brage <bragearn@stud.ntnu.no>
2020-03-12 14:20:11 +0100

    added some tests #13

--- src/inpaiting.py
+++ src/inpaiting.py
@@ -7,6 +7,7 @@ class inpait(loader.ImageLoader):
 		loader.ImageLoader.__init__(self, path, color)
 
 		self.alpha = 0.25
+		self.mask = None
 #		self.mask = self.destroy_information()
 
 	def destroy_information(self, strength=2):
@@ -32,5 +33,8 @@ class inpait(loader.ImageLoader):
 		self.data = (self.data * (self.mask)) + abs(self.original_data * (1 - self.mask))
 
 	def fit(self, epochs=1):
+		if(self.mask is None):
+			# TODO: In the future you should be able to set a mask manually. 
+			raise Exception("You need to destroy infomation in the image before you run this function")
 		for i in range(epochs):
 			self.eksplisitt()
