a40d690a3291c3c58884d626060916c87a8e6cb6
Brage <bragearn@stud.ntnu.no>
2020-03-21 21:43:03 +0100

    wrote asome basic matlab code to run the gsolve function #12

--- src/files/gsolve.m
+++ src/files/gsolve.m
@@ -1,21 +1,33 @@
 % from http://www.pauldebevec.com/Research/HDR/debevec-siggraph97.pdf
-function [g,lE] = gsolve(Z,B,l,w) n = 256;
-A = zeros(size(Z,1)*size(Z,2)+n+1,n+size(Z,1)); b = zeros(size(A,1),1);
+function [g,lE] = gsolve(Z,B,l,w) 
+disp(Z);
+%exit(0);
+n = 256;
+A = zeros(size(Z,1)*size(Z,2)+n+1,n+size(Z,1)); 
+b = zeros(size(A,1),1);
 %% Include the data−fitting equations
+disp(size(Z,1) +","+ size(Z,2)+","+n+","+1+",," + n+","+size(Z,1));
+disp("" + size(Z))
+disp("" + size(A))
+disp("" + size(b))
 k = 1;
 for i=1:size(Z,1)
 	for j=1:size(Z,2)
 		wij = w(Z(i,j)+1);
-		A(k,Z(i,j)+1) = wij; A(k,n+i) = −wij; k=k+1;
-		b(k,1) = wij * B(i,j);
+		A(k,Z(i,j)+1) = wij; 
+		A(k,n+i) = -wij; 
+        disp(i +","+ j);
+		b(k,1) = wij * B(j);
+        k=k+1;
 	end 
 end
+disp(b);
 
-A(k,129) = 1
+A(k,129) = 1;
 k=k+1;
 
-for i=1:n−2
-	A(k,i)=l*w(i+1); A(k,i+1)=−2*l*w(i+1); A(k,i+2)=l*w(i+1); k=k+1;
+for i=1:n-2
+	A(k,i)=l*w(i+1); A(k,i+1)=-2*l*w(i+1); A(k,i+2)=l*w(i+1); k=k+1;
 end
 
 x = A\b;
